67b8b74356874bc25ee62c7094e7f86c17f98f51
PITTAM VENKATA AJAY KUMAR REDDY <pittam1234@gmail.com>
2026-07-11 13:33:48 +0530

    Create OpenApiConfig.java

---
+++ heplerhub-backend/src/main/java/com/helperhub/config/OpenApiConfig.java
@@ -0,0 +1,40 @@
+package com.helperhub.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI helperHubOpenAPI() {
+
+        String securitySchemeName = "Bearer Authentication";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("HelperHub API")
+                        .version("1.0")
+                        .description("HelperHub Service Booking REST API"))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}

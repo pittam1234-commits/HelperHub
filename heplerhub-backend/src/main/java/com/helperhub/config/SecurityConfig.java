3cf2483c8b0bff202f40b5e4f7b77570ef848745
PITTAM VENKATA AJAY KUMAR REDDY <pittam1234@gmail.com>
2026-07-11 12:00:24 +0530

    Update SecurityConfig.java

--- heplerhub-backend/src/main/java/com/helperhub/config/SecurityConfig.java
+++ heplerhub-backend/src/main/java/com/helperhub/config/SecurityConfig.java
@@ -5,13 +5,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+<<<<<<< HEAD
+=======
 
+>>>>>>> 34a8170f4808bd5747bff4a799badc0cdd572b62
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,15 +28,25 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
 
         http
+<<<<<<< HEAD
+=======
                 // Disable CSRF
+>>>>>>> 34a8170f4808bd5747bff4a799badc0cdd572b62
                 .csrf(csrf -> csrf.disable())
 
                 // Stateless Session
                 .sessionManagement(session ->
+<<<<<<< HEAD
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+=======
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+>>>>>>> 34a8170f4808bd5747bff4a799badc0cdd572b62
                 )
 
                 // Public Endpoints
@@ -44,11 +55,13 @@ public class SecurityConfig {
                                 "/",
                                 "/error",
                                 "/auth/**",
+<<<<<<< HEAD
+=======
 
+>>>>>>> 34a8170f4808bd5747bff4a799badc0cdd572b62
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
-                                "/v3/api-docs",
                                 "/swagger-resources/**",
                                 "/webjars/**"
                         ).permitAll()
@@ -56,7 +69,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
 
+<<<<<<< HEAD
+=======
                 // Return 401 instead of Browser Login Popup
+>>>>>>> 34a8170f4808bd5747bff4a799badc0cdd572b62
                 .exceptionHandling(exception ->
                         exception.authenticationEntryPoint(
                                 (request, response, authException) ->

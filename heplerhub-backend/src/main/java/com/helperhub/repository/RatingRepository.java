83f8fb5a35e7f84e76ebfe52be94ccaf5e42eb18
PITTAM VENKATA AJAY KUMAR REDDY <pittam1234@gmail.com>
2026-07-13 11:05:10 +0530

    Update RatingRepository.java

--- heplerhub-backend/src/main/java/com/helperhub/repository/RatingRepository.java
+++ heplerhub-backend/src/main/java/com/helperhub/repository/RatingRepository.java
@@ -2,11 +2,15 @@ package com.helperhub.repository;
 
 import com.helperhub.entity.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface RatingRepository extends JpaRepository<Rating, Long> {
+@Repository
+public interface RatingRepository
+        extends JpaRepository<Rating, Long> {
 
     List<Rating> findByWorkerId(Long workerId);
 
+    boolean existsByBookingId(Long bookingId);
 }
